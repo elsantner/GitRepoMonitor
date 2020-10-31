290ffd6ac025e51ebeb73b4079e6511bef49bd1c
Elias <elias.santner@edu.htl-villach.at>
2020-10-31 11:50:09 +0100

    Implement visual representation of invalid repo path
    - whenever an invalid path is detected an attention sign is displayed next to the entry

--- gui/src/main/java/at/aau/ainf/gitrepomonitor/gui/RepositoryInformationCellFactory.java
+++ gui/src/main/java/at/aau/ainf/gitrepomonitor/gui/RepositoryInformationCellFactory.java
@@ -3,11 +3,15 @@ package at.aau.ainf.gitrepomonitor.gui;
 import at.aau.ainf.gitrepomonitor.files.FileManager;
 import at.aau.ainf.gitrepomonitor.files.RepositoryInformation;
 import at.aau.ainf.gitrepomonitor.gui.editrepo.ControllerEditRepo;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -22,19 +26,25 @@ public class RepositoryInformationCellFactory
 
     @Override
     public ListCell<RepositoryInformation> call(ListView<RepositoryInformation> param) {
-        ListCell<RepositoryInformation> cell = new ListCell<>() {
-            @Override
-            protected void updateItem(RepositoryInformation item, boolean empty) {
-                super.updateItem(item, empty);
+        ListCell<RepositoryInformation> cell = new RepositoryInformationListViewCell();
 
-                if (empty || item == null) {
-                    setText(null);
-                } else {
-                    setText(item.toString());
-                }
+        // ctx menu only on non-empty list entries
+        cell.emptyProperty().addListener((obs, wasEmpty, isNowEmpty) -> {
+            if (isNowEmpty) {
+                cell.setContextMenu(null);
+            } else {
+                cell.setContextMenu(getContextMenu(cell));
             }
-        };
+        });
+        return cell;
+    }
 
+    /**
+     * Returns a setup & configured ContextMenu for the given cell
+     * @param cell The cell for which the context menu is created
+     * @return setup ContextMenu
+     */
+    private ContextMenu getContextMenu(ListCell<RepositoryInformation> cell) {
         ContextMenu contextMenu = new ContextMenu();
         MenuItem editItem = new MenuItem();
         editItem.setText(ResourceStore.getResourceBundle().getString("ctxmenu.edit"));
@@ -61,16 +71,7 @@ public class RepositoryInformationCellFactory
         });
 
         contextMenu.getItems().addAll(editItem, deleteItem, showInExplorerItem);
-
-        // ctx menu only on non-empty list entries
-        cell.emptyProperty().addListener((obs, wasEmpty, isNowEmpty) -> {
-            if (isNowEmpty) {
-                cell.setContextMenu(null);
-            } else {
-                cell.setContextMenu(contextMenu);
-            }
-        });
-        return cell;
+        return contextMenu;
     }
 
     private void openEditWindow(RepositoryInformation repo) throws IOException {
@@ -88,5 +89,44 @@ public class RepositoryInformationCellFactory
         stage.show();
         stage.setMinWidth(stage.getWidth());
         stage.setMinHeight(stage.getHeight());
+    }
+
+    private static class RepositoryInformationListViewCell extends ListCell<RepositoryInformation> {
+        @FXML
+        private Label lblName;
+        @FXML
+        private HBox container;
+        @FXML
+        private ImageView iconAttention;
+
+        private FXMLLoader loader;
+
+        @Override
+        protected void updateItem(RepositoryInformation item, boolean empty) {
+            super.updateItem(item, empty);
+
+            // clear entry if no/empty item (required for clean refresh of listview)
+            if (empty || item == null) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                // load fxml if not loaded already
+                if (loader == null) {
+                    loader = new FXMLLoader(getClass().getResource("/at/aau/ainf/gitrepomonitor/gui/repo_list_item.fxml"));
+                    loader.setController(this);
+
+                    try {
+                        loader.load();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                // fill display elements with data
+                lblName.setText(item.toString());
+                iconAttention.setVisible(!item.isPathValid());
+                setGraphic(container);
+            }
+        }
     }
 }
